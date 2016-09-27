94ef5248916f604db906cb01d24bc415295f0f8b
Ryan Casperson <casperson.ryan@gmail.com>
2016-09-27 17:54:08 -0400

    Outline the structure for contractors and temporaries in IndependentWorker class

--- class-model/employees.py
+++ class-model/employees.py
@@ -14,11 +14,21 @@ class Employee(Object):
     def get_yearly_vacation(self):
         return self.yearly_vacation
 
-class IndependantWorker(Employee):
+class IndependentWorker(Employee):
+    def __init__(self, first_name, last_name, pay_rate, agency_name, label):
+        default_yearly_vacation = 0
+        Employee.__init__(self, first_name, last_name, pay_rate, default_yearly_vacation)
+        self.agency_name = agency_name
+        self.label = label
+
+    def get_name(self):
+        return "{0}, {1} [ {2} ]".format(self.last_name, self.first_name, self.label)
+
+    def get_agency(self):
+        return self.agency_name
+
+class Contractor(IndependentWorker):
     pass
 
-class Contractor(IndependantWorker):
-    pass
-
-class Temporary(IndependantWorker):
+class Temporary(IndependentWorker):
     pass
