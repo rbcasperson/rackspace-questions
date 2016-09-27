8477176f4f0d2934e647990c068d92d11cf8f112
Ryan Casperson <casperson.ryan@gmail.com>
2016-09-27 18:08:34 -0400

    Fix object inheritance typo in Employee

--- class-model/employees.py
+++ class-model/employees.py
@@ -1,4 +1,4 @@
-class Employee(Object):
+class Employee(object):
     def __init__(self, first_name, last_name, pay_rate, yearly_vacation):
         self.first_name = first_name
         self.last_name = last_name
